3ef086916a38191ae5e7ded4319f7189552271f6
Waritsars <warissara.0039@gmail.com>
2020-04-23 14:28:59 +0700

    เพิ่มค้นหา country

---
+++ country.js
@@ -0,0 +1,34 @@
+$(function(){
+    
+    var urlParams = new URLSearchParams(window.location.search);
+    console.log(urlParams.get('country'));
+
+    var country = urlParams.get('country');
+    $("#country").html(country);
+
+    var url = "https://pomber.github.io/covid19/timeseries.json";
+
+    $.getJSON(url, function (result) {
+
+        var selectedCountry = result[country];
+        console.log(selectedCountry);
+
+        for(var i=0;i<selectedCountry.length;i++){
+           
+            var row = `<tr>
+            <th scope="row">${selectedCountry[i].date}</th>
+            <td>${selectedCountry[i].confirmed}</td>
+            <td>${selectedCountry[i].deaths}</td>
+            <td>${selectedCountry[i].recovered}</td>
+          </tr>`
+
+          $("#data").append(row);
+
+        }
+        
+
+    });
+
+
+
+})
